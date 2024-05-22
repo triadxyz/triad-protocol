4b1027bb30282f7b3e9d139070f19f3599fede3d
Davi Cardoso <davicardoso.2103@gmail.com>
2024-05-22 19:48:21 +0300

    fix: update micro lamports

--- sdk/src/vault.ts
+++ sdk/src/vault.ts
@@ -98,7 +98,7 @@ export default class Vault {
 
       const instructions: TransactionInstruction[] = [
         ComputeBudgetProgram.setComputeUnitPrice({
-          microLamports: 25000
+          microLamports: 60000
         })
       ]
 
@@ -191,7 +191,7 @@ export default class Vault {
 
       const instructions: TransactionInstruction[] = [
         ComputeBudgetProgram.setComputeUnitPrice({
-          microLamports: 25000
+          microLamports: 60000
         })
       ]
 
