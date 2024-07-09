14c466093fc7e3ad7d11b14f4d058ada0618da73
dannpl <dancardosobr@gmail.com>
2024-07-09 14:24:15 -0300

    remove comments

--- programs/triad-protocol/src/state/ticker.rs
+++ programs/triad-protocol/src/state/ticker.rs
@@ -2,21 +2,13 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct Ticker {
-    /// timestamp of the creation of the ticker
     pub init_ts: i64,
-    /// timestamp of the last update of the ticker
     pub updated_ts: i64,
-    /// The bump for the ticker pda
     pub bump: u8,
-    /// authority for the ticker
     pub authority: Pubkey,
-    /// name of the ticekt
     pub name: String,
-    /// protocol program id like dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH to get data info
     pub protocol_program_id: Pubkey,
-    /// ticker price
     pub price: u64,
-    /// Vault PDA
     pub vault: Pubkey,
 }
 
@@ -32,11 +24,7 @@ pub struct UpdateTickerPriceArgs {
 }
 
 impl Ticker {
-    /// static prefix seed string used to derive the PDAs
     pub const PREFIX_SEED: &'static [u8] = b"ticker";
 
-    /// total on-chain space needed to allocate the account
-    pub const SPACE: usize =
-        // anchor descriminator + all static variables
-        8 + std::mem::size_of::<Self>();
+    pub const SPACE: usize = 8 + std::mem::size_of::<Self>();
 }
--- programs/triad-protocol/src/state/vault.rs
+++ programs/triad-protocol/src/state/vault.rs
@@ -2,33 +2,19 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct Vault {
-    /// The bump for the vault pda
     pub bump: u8,
-    /// authority for the vault
     pub authority: Pubkey,
-    /// name of the vault
     pub name: String,
-    /// token account for the vault e.g. tDRIFT
     pub token_account: Pubkey,
-    /// ticker address
     pub ticker_address: Pubkey,
-    /// lifetime total deposited
     pub total_deposited: u64,
-    /// lifetime total withdrawn
     pub total_withdrawn: u64,
-    /// timestamp vault initialized
     pub init_ts: i64,
-    /// lifetime net deposits
     pub net_deposits: u128,
-    /// lifetime net withdraws
     pub net_withdraws: u128,
-    /// Long bet balance
     pub long_balance: u64,
-    /// Short bet balance
     pub short_balance: u64,
-    /// Opened long positions
     pub long_positions_opened: u64,
-    /// Opened short positions
     pub short_positions_opened: u64,
 }
 
@@ -44,17 +30,9 @@ pub struct ClosePositionArgs {
 }
 
 impl Vault {
-    /// static prefix seed string used to derive the PDAs
     pub const PREFIX_SEED: &'static [u8] = b"vault";
 
-    /// total on-chain space needed to allocate the account
-    pub const SPACE: usize =
-        // anchor descriminator + all static variables
-        8 + std::mem::size_of::<Self>();
-
-    pub fn get_vault_signer_seeds<'a>(ticker: &'a Pubkey, bump: &'a u8) -> [&'a [u8]; 3] {
-        [b"vault".as_ref(), ticker.as_ref(), bytemuck::bytes_of(bump)]
-    }
+    pub const SPACE: usize = 8 + std::mem::size_of::<Self>();
 
     pub const PREFIX_SEED_VAULT_TOKEN_ACCOUNT: &'static [u8] = b"vault_token_account";
 }
