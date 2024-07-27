6b2b07cc1b9bd609b52468df4c51d373e06af491
dannpl <dancardosobr@gmail.com>
2024-07-27 14:36:04 -0300

    update test

--- sdk/src/test.ts
+++ sdk/src/test.ts
@@ -13,23 +13,6 @@ const connection = new Connection(rpc_file.toString(), 'confirmed')
 const wallet = new Wallet(keypair)
 const triadProtocol = new TriadProtocol(connection, wallet)
 
-const requestWithdraw = async () => {
-  const response = await triadProtocol.stake.requestWithdraw(
-    {
-      wallet: wallet.publicKey,
-      name: 'Triad 873',
-      mint: new PublicKey('FnLsRRfs7Ghhiwybr7fzZhFyfQEKQbxCJMtLghjiKLHy'),
-      stakeVault: STAKE_SEASON
-    },
-    {
-      microLamports: 10000,
-      skipPreflight: true
-    }
-  )
-
-  console.log(response)
-}
-
 const updateStakeVaultStatus = async () => {
   const response = await triadProtocol.stake.updateStakeVaultStatus(
     {
@@ -47,23 +30,6 @@ const updateStakeVaultStatus = async () => {
   console.log(response)
 }
 
-const withdraw = async () => {
-  const response = await triadProtocol.stake.withdrawStake(
-    {
-      wallet: wallet.publicKey,
-      name: 'Triad 2807',
-      mint: new PublicKey('A4fu2s6bCbKAveVgACkxpMgtJUHqoRJsTyzRp1Jp8nuE'),
-      stakeVault: STAKE_SEASON
-    },
-    {
-      microLamports: 10000,
-      skipPreflight: true
-    }
-  )
-
-  console.log(response)
-}
-
 const getStake = async () => {
   const response = await triadProtocol.stake.getStakeByWallet(
     new PublicKey('BCTdjdcjMiECGFbF5Ps15yjLRPzy5YZGJNa4VdGRbhjB'),
@@ -74,23 +40,6 @@ const getStake = async () => {
 
   console.log(response)
   console.log(stakeVaults)
-}
-
-const claimStakeRewardsV1 = async () => {
-  const response = await triadProtocol.stake.claimStakeRewards(
-    {
-      wallet: wallet.publicKey,
-      mint: new PublicKey('t3DohmswhKk94PPbPYwA6ZKACyY3y5kbcqeQerAJjmV'),
-      stakeVault: STAKE_SEASON,
-      nftName: 'Triad 2807'
-    },
-    {
-      skipPreflight: true,
-      microLamports: 10000
-    }
-  )
-
-  console.log(response)
 }
 
 const stake = async () => {
